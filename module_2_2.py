fb9bf95460eed2525d95ddb831eda00e6633c37a
OksanaM22 <oksamar22@gmail.com>
2024-10-21 17:09:06 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -2,7 +2,7 @@ first=int(input("Введите первое число: "))
 second =int(input("Введите второе число: "))
 third =int(input("Введите третье число: "))
 if first==second==third :
-    print(first,          second,          third)
+    rint(3)
 elif first==second or first==third or second==third:
     print(2)
 else:
