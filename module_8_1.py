786ae4c7a32718a349f5867a9e85c009b4e5c94a
OksanaM22 <oksamar22@gmail.com>
2024-12-23 20:58:40 +0300

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -1,28 +1,12 @@
 # Программистам всё можно
 def add_everything_up(a, b):
     try:
-        result = a + b
-        if isinstance(a,(int, float)) and  isinstance(b, (int, float)):
-            return result
-        elif isinstance(a, str) and isinstance(b, str):
-            return result
+        return a + b
 
-    except TypeError as exc:
-        if isinstance(a, (int,float)) and isinstance(b, str):
-            return f'{a}{b}'
-        elif isinstance(a, str) and isinstance(b, (int, float)):
-            return f'{a}{b}'
+    except TypeError:
+        return str(a)+str(b)
 
 
 print (add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
-
-
-
-
-
-
-
-
-
